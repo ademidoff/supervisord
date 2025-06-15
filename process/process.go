93832dbb712cd387a4727e89e3ab81b2c3951c14
Alex Demidoff <alex@demidoff.me>
2025-06-15 12:49:27 +0300

    fix: error in process.go

--- process/process.go
+++ process/process.go
@@ -507,7 +507,7 @@ func (p *Process) waitForExit(startSecs int64) {
 
 // fail to start the program
 func (p *Process) failToStartProgram(reason string, finishCb func()) {
-	log.WithFields(log.Fields{"program": p.GetName()}).Errorf(reason)
+	log.WithFields(log.Fields{"program": p.GetName()}).Error(reason)
 	p.changeStateTo(Fatal)
 	finishCb()
 }
